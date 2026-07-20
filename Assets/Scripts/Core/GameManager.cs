5fdf9e6386d703a3b8eff24dc1f1496ff0aa5851
Marcus Boehm <m.adolph@gmx.net>
2026-07-20 21:41:14 +0200

    2026-07-20 v 7

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,4 +1,5 @@
 using DM.Dungeon;
+using DM.Rendering;
 using UnityEngine;
 
 namespace DM.Core
@@ -6,10 +7,15 @@ namespace DM.Core
   public class GameManager : MonoBehaviour
   {
     private DungeonMap _dungeonMap;
+    private DungeonRenderer _dungeonRenderer;
 
     private void Awake()
     {
       _dungeonMap = new DungeonMap(10, 6);
+
+      _dungeonRenderer = gameObject.AddComponent<DungeonRenderer>();
+
+      _dungeonRenderer.Render(_dungeonMap);
 
       Debug.Log($"Dungeon created: {_dungeonMap.Width} x {_dungeonMap.Height}");
       Debug.Log(_dungeonMap.BuildDebugMap());
