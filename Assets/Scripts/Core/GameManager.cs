285af181f2247db24866a8881d086b0f219040dc
Marcus Boehm <m.adolph@gmx.net>
2026-07-20 20:00:09 +0200

    2026-07-20 v 2

---
+++ Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace DM.Core
+{
+  public class GameManager : MonoBehaviour
+  {
+    private void Awake()
+    {
+      Debug.Log("Dungeon Master engine started.");
+    }
+  }
+}
