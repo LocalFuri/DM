7c6b0232d8a24edd1f01ebeeadc1c9f89b319cb8
Marcus Boehm <m.adolph@gmx.net>
2026-07-20 20:44:01 +0200

    2026-07-20 v 5 - milestone engine foundation

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,12 +1,18 @@
+using DM.Dungeon;
 using UnityEngine;
 
 namespace DM.Core
 {
   public class GameManager : MonoBehaviour
   {
+    private DungeonMap _dungeonMap;
+
     private void Awake()
     {
-      Debug.Log("Dungeon Master engine started.");
+      _dungeonMap = new DungeonMap(10, 6);
+
+      Debug.Log($"Dungeon created: {_dungeonMap.Width} x {_dungeonMap.Height}");
+      
     }
   }
 }
--- Assets/Scripts/Dungeon/DungeonMap.cs
+++ Assets/Scripts/Dungeon/DungeonMap.cs
@@ -2,6 +2,46 @@ namespace DM.Dungeon
 {
   public class DungeonMap
   {
+    private readonly DungeonTile[,] _tiles;
 
+    public int Width { get; }
+    public int Height { get; }
+
+    public DungeonMap(int width, int height)
+    {
+      Width = width;
+      Height = height;
+
+      _tiles = new DungeonTile[width, height];
+
+      CreateTestDungeon();
+    }
+
+    public DungeonTile GetTile(int x, int y)
+    {
+      return _tiles[x, y];
+    }
+
+    private void CreateTestDungeon()
+    {
+      for (int y = 0; y < Height; y++)
+      {
+        for (int x = 0; x < Width; x++)
+        {
+          bool isBorder =
+              x == 0 ||
+              y == 0 ||
+              x == Width - 1 ||
+              y == Height - 1;
+
+          _tiles[x, y] = new DungeonTile
+          {
+            Type = isBorder
+                  ? DungeonTileType.Wall
+                  : DungeonTileType.Floor
+          };
+        }
+      }
+    }
   }
 }
