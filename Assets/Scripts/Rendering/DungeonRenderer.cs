fda6af85e09a73b2218dd86159856a95f91fcf76
Marcus Boehm <m.adolph@gmx.net>
2026-07-20 21:46:47 +0200

    2026-07-20 v 8

--- Assets/Scripts/Rendering/DungeonRenderer.cs
+++ Assets/Scripts/Rendering/DungeonRenderer.cs
@@ -5,63 +5,9 @@ namespace DM.Rendering
 {
   public class DungeonRenderer : MonoBehaviour
   {
-    private const float TileSize = 1f;
-
     public void Render(DungeonMap map)
     {
-      Shader shader = Shader.Find("Universal Render Pipeline/Unlit");
-
-      if (shader == null)
-      {
-        Debug.LogError("URP Unlit shader was not found.");
-        return;
-      }
-
-      Material wallMaterial = new Material(shader)
-      {
-        color = Color.gray
-      };
-
-      Material floorMaterial = new Material(shader)
-      {
-        color = new Color(0.15f, 0.15f, 0.15f)
-      };
-
-      for (int y = 0; y < map.Height; y++)
-      {
-        for (int x = 0; x < map.Width; x++)
-        {
-          DungeonTile tile = map.GetTile(x, y);
-
-          GameObject tileObject =
-              GameObject.CreatePrimitive(PrimitiveType.Quad);
-
-          tileObject.name = $"Tile_{x}_{y}";
-          tileObject.transform.SetParent(transform);
-
-          tileObject.transform.position = new Vector3(
-              x * TileSize,
-              y * TileSize,
-              0f
-          );
-
-          MeshRenderer tileRenderer =
-              tileObject.GetComponent<MeshRenderer>();
-
-          tileRenderer.sharedMaterial =
-              tile.Type == DungeonTileType.Wall
-                  ? wallMaterial
-                  : floorMaterial;
-
-          Collider tileCollider =
-              tileObject.GetComponent<Collider>();
-
-          if (tileCollider != null)
-          {
-            Destroy(tileCollider);
-          }
-        }
-      }
+      Debug.Log("DungeonRenderer started.");
     }
   }
 }
