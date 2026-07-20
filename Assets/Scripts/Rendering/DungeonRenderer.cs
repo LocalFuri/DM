978f392a28805bd4fd44ba0c1f785fcd78b37fa3
Marcus Boehm <m.adolph@gmx.net>
2026-07-20 21:52:37 +0200

    2026-07-20 v 8 mini miletone

--- Assets/Scripts/Rendering/DungeonRenderer.cs
+++ Assets/Scripts/Rendering/DungeonRenderer.cs
@@ -8,6 +8,11 @@ namespace DM.Rendering
     public void Render(DungeonMap map)
     {
       Debug.Log("DungeonRenderer started.");
+
+      GameObject cube =
+          GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+      cube.transform.position = Vector3.zero;
     }
   }
 }
